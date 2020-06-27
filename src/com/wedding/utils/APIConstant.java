0881ee0f1c6aa00b58a6e604441e6e5ca6f00662
uyenphuong <dinhngocuyenphuong1802@gmail.com>
2020-06-27 09:24:11 +0700

    change API url

--- src/com/wedding/utils/APIConstant.java
+++ src/com/wedding/utils/APIConstant.java
@@ -9,49 +9,94 @@ public class APIConstant {
 //	public static final String API_service_add = "http://wedding.j.layershift.co.uk/service/add";
 //	public static final String API_service_delete = "http://wedding.j.layershift.co.uk/service/delete";
 	
+//	// RESTFUL of API food
+//	public static final String API_food_get = "http://localhost:8082/WeddingManagementBackend/food";
+//	public static final String API_food_add = "http://localhost:8082/WeddingManagementBackend/food/add";
+//	public static final String API_food_delete = "http://localhost:8082/WeddingManagementBackend/food/delete";
+//	public static final String API_food_update = "http://localhost:8082/WeddingManagementBackend/food/update";
+//	
+//	// RESTFUL of API service
+//	public static final String API_service_get = "http://localhost:8082/WeddingManagementBackend/service";
+//	public static final String API_service_add = "http://localhost:8082/WeddingManagementBackend/service/add";
+//	public static final String API_service_delete = "http://localhost:8082/WeddingManagementBackend/service/delete";
+//	public static final String API_service_update = "http://localhost:8082/WeddingManagementBackend/service/update";
+//	
+//	// RESTFUL of API lobby
+//	public static final String API_lobby_get = "http://localhost:8082/WeddingManagementBackend/lobby";
+//	public static final String API_lobby_add = "http://localhost:8082/WeddingManagementBackend/lobby/add";
+//	public static final String API_lobby_delete = "http://localhost:8082/WeddingManagementBackend/lobby/delete";
+//	public static final String API_lobby_update = "http://localhost:8082/WeddingManagementBackend/lobby/update";
+//	public static final String API_lobby_check = "http://localhost:8082/WeddingManagementBackend/lobby/check";
+//	
+//	// RESTFUL of API employee
+//	public static final String API_employee_get = "http://localhost:8082/WeddingManagementBackend/employee";
+//	public static final String API_employee_add = "http://localhost:8082/WeddingManagementBackend/employee/add";
+//	public static final String API_employee_delete = "http://localhost:8082/WeddingManagementBackend/employee/delete";
+//	public static final String API_employee_update = "http://localhost:8082/WeddingManagementBackend/employee/update";
+//	public static final String API_employee_resetpassword = "http://localhost:8082/WeddingManagementBackend/employee/resetpassword";
+//	public static final String API_employee_username = "http://localhost:8082/WeddingManagementBackend/employee/username";
+//	
+//	// RESTFUL of API change password
+//	public static final String API_employee_changepassword = "http://localhost:8082/WeddingManagementBackend/updatepassword";
+//	
+//	// RESTFUL of API typelobby
+//	public static final String API_typelobby_get = "http://localhost:8082/WeddingManagementBackend/typelobby";
+//	
+//	// RESTFUL of API reservation
+//	public static final String API_reservation = "http://localhost:8082/WeddingManagementBackend/reservation";
+//	public static final String API_reservation_add = "http://localhost:8082/WeddingManagementBackend/reservation/add";
+//	public static final String API_reservation_delete = "http://localhost:8082/WeddingManagementBackend/reservation/delete";
+//	public static final String API_reservation_update = "http://localhost:8082/WeddingManagementBackend/reservation/update";
+//	public static final String API_reservation_pay = "http://localhost:8082/WeddingManagementBackend/reservation/pay";
+//	
+//	// RESTFUL of API total revenue
+//	public static final String API_total_revenue = "http://localhost:8082/WeddingManagementBackend/totalrevenue";
+//	public static final String API_month_revenue = "http://localhost:8082/WeddingManagementBackend/monthrevenue";
+//	
+//	public static final String API_login = "http://localhost:8082/WeddingManagementBackend/login";
 	// RESTFUL of API food
-	public static final String API_food_get = "http://localhost:8082/WeddingManagementBackend/food";
-	public static final String API_food_add = "http://localhost:8082/WeddingManagementBackend/food/add";
-	public static final String API_food_delete = "http://localhost:8082/WeddingManagementBackend/food/delete";
-	public static final String API_food_update = "http://localhost:8082/WeddingManagementBackend/food/update";
-	
-	// RESTFUL of API service
-	public static final String API_service_get = "http://localhost:8082/WeddingManagementBackend/service";
-	public static final String API_service_add = "http://localhost:8082/WeddingManagementBackend/service/add";
-	public static final String API_service_delete = "http://localhost:8082/WeddingManagementBackend/service/delete";
-	public static final String API_service_update = "http://localhost:8082/WeddingManagementBackend/service/update";
-	
-	// RESTFUL of API lobby
-	public static final String API_lobby_get = "http://localhost:8082/WeddingManagementBackend/lobby";
-	public static final String API_lobby_add = "http://localhost:8082/WeddingManagementBackend/lobby/add";
-	public static final String API_lobby_delete = "http://localhost:8082/WeddingManagementBackend/lobby/delete";
-	public static final String API_lobby_update = "http://localhost:8082/WeddingManagementBackend/lobby/update";
-	public static final String API_lobby_check = "http://localhost:8082/WeddingManagementBackend/lobby/check";
-	
-	// RESTFUL of API employee
-	public static final String API_employee_get = "http://localhost:8082/WeddingManagementBackend/employee";
-	public static final String API_employee_add = "http://localhost:8082/WeddingManagementBackend/employee/add";
-	public static final String API_employee_delete = "http://localhost:8082/WeddingManagementBackend/employee/delete";
-	public static final String API_employee_update = "http://localhost:8082/WeddingManagementBackend/employee/update";
-	public static final String API_employee_resetpassword = "http://localhost:8082/WeddingManagementBackend/employee/resetpassword";
-	public static final String API_employee_username = "http://localhost:8082/WeddingManagementBackend/employee/username";
-	
-	// RESTFUL of API change password
-	public static final String API_employee_changepassword = "http://localhost:8082/WeddingManagementBackend/updatepassword";
-	
-	// RESTFUL of API typelobby
-	public static final String API_typelobby_get = "http://localhost:8082/WeddingManagementBackend/typelobby";
-	
-	// RESTFUL of API reservation
-	public static final String API_reservation = "http://localhost:8082/WeddingManagementBackend/reservation";
-	public static final String API_reservation_add = "http://localhost:8082/WeddingManagementBackend/reservation/add";
-	public static final String API_reservation_delete = "http://localhost:8082/WeddingManagementBackend/reservation/delete";
-	public static final String API_reservation_update = "http://localhost:8082/WeddingManagementBackend/reservation/update";
-	public static final String API_reservation_pay = "http://localhost:8082/WeddingManagementBackend/reservation/pay";
-	
-	// RESTFUL of API total revenue
-	public static final String API_total_revenue = "http://localhost:8082/WeddingManagementBackend/totalrevenue";
-	public static final String API_month_revenue = "http://localhost:8082/WeddingManagementBackend/monthrevenue";
-	
-	public static final String API_login = "http://localhost:8082/WeddingManagementBackend/login";
+		public static final String API_food_get = "http://wedding.j.layershift.co.uk/food";
+		public static final String API_food_add = "http://wedding.j.layershift.co.uk/food/add";
+		public static final String API_food_delete = "http://wedding.j.layershift.co.uk/food/delete";
+		public static final String API_food_update = "http://wedding.j.layershift.co.uk/food/update";
+		
+		// RESTFUL of API service
+		public static final String API_service_get = "http://wedding.j.layershift.co.uk/service";
+		public static final String API_service_add = "http://wedding.j.layershift.co.uk/service/add";
+		public static final String API_service_delete = "http://wedding.j.layershift.co.uk/service/delete";
+		public static final String API_service_update = "http://wedding.j.layershift.co.uk/service/update";
+		
+		// RESTFUL of API lobby
+		public static final String API_lobby_get = "http://wedding.j.layershift.co.uk/lobby";
+		public static final String API_lobby_add = "http://wedding.j.layershift.co.uk/lobby/add";
+		public static final String API_lobby_delete = "http://wedding.j.layershift.co.uk/lobby/delete";
+		public static final String API_lobby_update = "http://wedding.j.layershift.co.uk/lobby/update";
+		public static final String API_lobby_check = "http://wedding.j.layershift.co.uk/lobby/check";
+		
+		// RESTFUL of API employee
+		public static final String API_employee_get = "http://wedding.j.layershift.co.uk/employee";
+		public static final String API_employee_add = "http://wedding.j.layershift.co.uk/employee/add";
+		public static final String API_employee_delete = "http://wedding.j.layershift.co.uk/employee/delete";
+		public static final String API_employee_update = "http://wedding.j.layershift.co.uk/employee/update";
+		public static final String API_employee_resetpassword = "http://wedding.j.layershift.co.uk/employee/resetpassword";
+		public static final String API_employee_username = "http://wedding.j.layershift.co.uk/employee/username";
+		
+		// RESTFUL of API change password
+		public static final String API_employee_changepassword = "http://wedding.j.layershift.co.uk/updatepassword";
+		
+		// RESTFUL of API typelobby
+		public static final String API_typelobby_get = "http://wedding.j.layershift.co.uk/typelobby";
+		
+		// RESTFUL of API reservation
+		public static final String API_reservation = "http://wedding.j.layershift.co.uk/reservation";
+		public static final String API_reservation_add = "http://wedding.j.layershift.co.uk/reservation/add";
+		public static final String API_reservation_delete = "http://wedding.j.layershift.co.uk/reservation/delete";
+		public static final String API_reservation_update = "http://wedding.j.layershift.co.uk/reservation/update";
+		public static final String API_reservation_pay = "http://wedding.j.layershift.co.uk/reservation/pay";
+		
+		// RESTFUL of API total revenue
+		public static final String API_total_revenue = "http://wedding.j.layershift.co.uk/totalrevenue";
+		public static final String API_month_revenue = "http://wedding.j.layershift.co.uk/monthrevenue";
+		
+		public static final String API_login = "http://wedding.j.layershift.co.uk/login";
 }
